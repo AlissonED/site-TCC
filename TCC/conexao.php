4d9cc6ee5014dd81b594555beeffb33e7e4c07f0
Alisson Evangelista <108416106+AlissonED@users.noreply.github.com>
2024-11-09 09:07:40 -0300

    Update conexao.php

--- TCC/conexao.php
+++ TCC/conexao.php
@@ -2,9 +2,9 @@
 
 
 $servername = "localhost";
-$username = "id19693905_alisson";
-$password = "Ca_9404394043";
-$db = "id19693905_contatos";
+$username = "";
+$password = "";
+$db = "";
 
 
 $conn = new mysqli($servername, $username, $password, $db);
